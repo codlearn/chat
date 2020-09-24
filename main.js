6331ae25ee989998c24c75682201af4969bb218f
Ismael Fermandez <ismael.7087@gmail.com>
2020-09-24 10:26:41 -0500

    second commit

--- main.js
+++ main.js
@@ -1,16 +1,8 @@
 /***
  *  user api https://randomuser.me/api/ for list users
- *
- *
- *
  ***/
 
 (async function load() {
-  /* async function getData(params) {
-    const response = await fetch(`https://reqres.in/api/${params}`);
-    const { data } = await response.json();
-    return data ? data : null;
-  } */
   const size = 768;
   async function getData(profile = false) {
     const response = await fetch(`https://randomuser.me/api/?results=10`);
@@ -20,11 +12,44 @@
     }
     return results ? results : null;
   }
-  async function profile() {
-    const { name, picture } = await getData(true);
-    //console.log(name, picture);
+
+  /* load messages */
+  const get_messages = async () => {
+    const response = await fetch("./data.json");
+    const data = await response.json();
+    return data;
+  };
+  let $messages = document.querySelector(".message-body");
+  function scroll() {    
+    $messages.scrollTop = $messages.scrollHeight;
+    window.scrollTo(0, $messages.scrollHeight);
   }
-  profile();
+  let content_messages = document.querySelector(".message-content");
+
+  async function load_messages(avatar) {
+    const res = await get_messages();
+    content_messages.innerHTML = "";
+    res.map((item, index) => {
+      let element = `
+      <div class="message-item ${item.type}">
+          <div class="avatar">
+              <img height="30" src="${
+                item.type == "sent" ? avatar : item.avatar
+              }" />
+          </div>
+          <div class="content">
+              <span> ${item.date} </span>
+              <p> ${item.message} </p>
+          </div>
+      </div>`;
+
+      content_messages.append(CreateTemplate(element));
+    });
+    scroll();
+  }
+  if (content_messages) {
+    load_messages();
+  }
 
   function CreateTemplate(htmlString) {
     const html = document.implementation.createHTMLDocument();
@@ -32,11 +57,12 @@
     return html.body.children[0];
   }
 
-  function template_list_user(id,name, picture, index) {    
-    return `<div class="list-item ${index == 0 ? " active" : ""}" data-id="${id}">
+  function template_list_user(id, name, picture, index) {
+    return `<div class="list-item ${
+      index == 0 ? " active" : ""
+    }" data-id="${id}">
       <div class="avatar">
-        ${index < 3 ? '<span class="active"></span>' : ""}
-        ${index >= 3 && index <= 5 ? "<span></span>" : ""}
+        ${index < 3 ? '<span class="active"></span>' : ""}        
         <img
           height="35"
           src="${picture.medium}"
@@ -55,13 +81,12 @@
   let contactList = document.querySelector("#contact-list");
   if (contactList != null) {
     userList.map((element, index) => {
-      const {login, name, picture } = element;
+      const { login, name, picture } = element;
       const item = template_list_user(login.uuid, name, picture, index);
       contactList.append(CreateTemplate(item));
     });
   }
   let main = document.querySelector("main");
-
   function resize() {
     if (window.innerWidth <= size) {
       main.classList.add("fixed");
@@ -76,13 +101,22 @@
       main.classList.toggle("active");
     }
   };
-  const activeMessage = (element)=>{
-    let img = element.querySelector('.avatar>img').src;
+
+  const activeMessage = (element) => {
+    let img = element.querySelector(".avatar>img").src;
     let name = element.querySelector(".info>strong").textContent;
+    let status = element.querySelector(".avatar>span");
+    load_messages(img);
+
     document.querySelector(".message-info>.avatar>img").src = img;
     document.querySelector(".message-info>.info>span").textContent = name;
-    console.log(img,name)
-  }
+    if (status != null) {
+      document.querySelector(".message-info>.info>small").textContent = name;
+    } else {
+      document.querySelector(".message-info>.info>small").remove();
+    }
+  };
+
   let back = document.querySelector(".back");
   back.addEventListener("click", () => showMessage());
   let items = document.querySelectorAll(".list-item");
@@ -94,15 +128,49 @@
 
   function selecList(element) {
     let $item = document.querySelector(".list-item.active");
-    if(element){
+    if (element) {
       $item.classList.remove("active");
       element.classList.add("active");
       showMessage();
       activeMessage(element);
-    } else { 
+    } else {
       showMessage();
       activeMessage($item);
-    }    
+    }
   }
-  selecList()
+  selecList();
+  function get_time() {
+    let t = new Date();
+    let h = t.getHours();
+    let min = t.getMinutes() < 10 ? "0" + t.getMinutes() : t.getMinutes();
+    min = (h<12)? min + " am": min+" pm";    
+    return h +":"+min;
+  }
+  // add new messages
+  function new_message(img, message) {
+    let element = `
+      <div class="message-item received">
+          <div class="avatar">
+              <img height="30" src="${img}" />
+          </div>
+          <div class="content">
+              <span> ${get_time()} </span>
+              <p> ${message} </p>
+          </div>
+      </div>`;
+    return CreateTemplate(element);
+  }
+  let input_message = document.querySelector("input#emojis");
+  let btn_send = document.getElementById("btn-send");
+  let profile_avatar = document.querySelector("#user-info>.avatar>img").src;
+  btn_send.addEventListener("click", function () {
+    add_message();
+  });
+  function add_message() {
+    content_messages.append(new_message(profile_avatar, input_message.value));
+    input_message.value = "";    
+    let emojis_editor = document.querySelector(".emojionearea-editor")
+    emojis_editor.innerHTML=''
+    scroll();
+  }
 })();
